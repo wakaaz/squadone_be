3bc035163e8ffd01083f2fb352cccedd2677da4c
danial-allomate <danial@allomate.com>
2022-03-22 11:23:31 +0500

    routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', function () {
-    return view('home');
+    return redirect('/home');
 });
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
